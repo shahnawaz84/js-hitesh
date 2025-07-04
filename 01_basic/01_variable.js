e3339012fc09f143b28ce503bbd4b3f5417e8bdd
shahnawaz <shahnawazshaukat84@gmail.com>
2025-07-04 11:40:05 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "shah@gmail.com"
+var accountPassword = 12345
+accountCity = "kolkata"
+
+
+// console.log("accountEmail");
+console.table({accountId,accountEmail,accountPassword})
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,10 @@
+"use strict"; // all java script code as newer version
+
+//alert (3+2) // hence we are using node js not browswe so alert variable not pop on screen. 
+
+let name = "shahnawaz"// string data always in doubele or single code""" ''
+let age = 40 // numeric data type without any code
+let isLoggedIn = true // Boolean data type answer in true or false
+//null is also a data type which mean no data value and its a standalone value
+//undefind is also a data type have some value but not decided
+//symbol is alos a data type we use its in react and figma for uniqueness purpose
