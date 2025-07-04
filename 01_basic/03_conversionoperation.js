a99a36f634c999752096c06b63c6672c2d1cebea
shahnawaz <shahnawazshaukat84@gmail.com>
2025-07-04 12:33:29 +0000

    conversion of data type

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,26 @@
+// let score = "33abc"
+// console.log(score);
+
+// let valueInNumber = Number(score)// for converting the 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// '33'convert to number and data type is number
+//'33aab' not convert and data type show is NaN
+// true vale is 1 and false value is 0
+// more
+
+// let isLoggedIn = 0
+// convert boolean data type to number
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+// true convert to 1 and false convert to 0 and vise versa
+
+// conert to srting to number data type
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
