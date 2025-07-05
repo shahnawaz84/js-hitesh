d25a5fc5d86b9c9695bcdba4281a3f5a9ea0c5f1
shahnawaz <shahnawazshaukat84@gmail.com>
2025-07-05 08:36:38 +0000

    data

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,20 @@
+console.log(2 < 1);
+console.log(2 >=1 );
+console.log(2 > 1);
+console.log(2==1);
+console.log(2 != 1);
+// above are basic convertsion with same data type so no issue to use it.
+
+
+console.log("2" >1 );
+console.log("02" > 1);
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0);
+console.log("2" <= 1);
+
+// above mention comparion not not done by strictly  and have sove some issue in result accuracy so please avoid it.
+// and data type not match dusring comarison by java script.
+//if data type are same then no issue.
+// its better to not use this compariosn in java we have to follow strict type data compariosn for better result.
+
